a469706a53e60015421438ec234758b0e5c00dc4
loksam <loksam.jp@gmail.com>
2018-11-30 00:05:54 +0900

    Initial

---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+
+const list = require('../lib/list');
+const search = require('../lib/search');
+
+/*******************************************/
+
+program
+  .version('0.0.1')
+  .parse(process.argv);
+
+program
+    .command('list') 
+    .alias('ls') 
+    .description('List installed package manager') 
+
+    .action(function () {
+        list();
+    });
+
+program
+    .command('search <package>') 
+    .alias('s') 
+    .description('Search package') 
+
+    .action(function (package) {
+        search(package);
+    });
+
+program.parse(process.argv);
+
+const NO_COMMAND_SPECIFIED = program.args.length === 0;
+if (NO_COMMAND_SPECIFIED) {
+  // e.g. display usage
+  program.help();
+}
